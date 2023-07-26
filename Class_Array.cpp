2aba5a29827b701f716847d2dbd621f53210aa22
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2023-07-26 11:15:22 +0400

    Commit with the template of the Class_Array.cpp

--- Class_Array.cpp
+++ Class_Array.cpp
@@ -1,14 +1,15 @@
 // Class_Array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+template <typename T>
+
 class Array
 {
 public:
     Array()
     {
         this->size = 5;
-        this->ptr = new int[this->size];
+        this->ptr = new T[this->size];
         for (int i = 0; i < this->size; i++)
             this->ptr[i] = rand()%100 + 1;
 
@@ -24,7 +25,7 @@ public:
     {
         this->size = array.size;
         
-        this->ptr = new int[size];
+        this->ptr = new T[size];
         for (int i = 0; i < size; i++)
             this->ptr[i] = array.ptr[i];       
     }
@@ -36,7 +37,7 @@ public:
             delete[] ptr;
             this->size = array.size;
 
-            this->ptr = new int[size];
+            this->ptr = new T[size];
             for (int i = 0; i < size; i++)
                 this->ptr[i] = array.ptr[i];
         }
@@ -64,7 +65,7 @@ public:
             this->ptr = nullptr;
 
             this->size = arr.size;
-            this->ptr = new int[size];
+            this->ptr = new T[size];
             for (int i = 0; i < size; i++)
                 this->ptr[i] = arr.ptr[i];
             
@@ -85,40 +86,32 @@ public:
             return this->ptr[0];
     }
 
-    int get_element(int i)
-    {
-        if (i >= 0 && i <= this->size)
-
-            return this->ptr[i];
-        else
-            return 0;     
-    }
-
     void print_array()
     {
         for (int i = 0; i < size; i++)
-            std::cout << this->ptr[i] << std::endl;
+            std::cout << this->ptr[i] << ",  " ;
+        std::cout << std::endl;
     }
 
 private:
-    int* ptr;
+    T* ptr;
     int size;
 
 };
 
 int main()
 {
-    Array array;
-    Array array2 = array;
-    Array array3;
+    Array<int> array;
+    Array<int> array2 = array;
+    Array<int> array3;
     array3 = array2;
 
     std::cout << array[4] << std::endl;
     
-    Array array5 = Array();
-    Array array6 = std::move(array5);
-
-    array3 = std::move(array2);
+    Array<char> array4;
+    Array<char> array5 = Array<char>();
+    
+    Array<char> array6 = std::move(Array<char>());
     return 0;
 }
 
